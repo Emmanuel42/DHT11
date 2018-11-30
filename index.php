680fd682531c409915947d750be94082739b867a
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-11-30 10:32:30 +0100

    Ajout fichiers index.php et main.css

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DHT11</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+
+	<h1>Température</h1>
+		
+
+<?php
+
+
+?>
+
+</body>
+</html>
+
+
