88716fa6b5734091b19885e2596957da5fce27e7
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-11-30 12:28:56 +0100

    affiche la temperature

--- index.php
+++ index.php
@@ -16,17 +16,18 @@
 $filename = "data.txt"; 
 $data_json = file_get_contents($filename) ; 
 $data = json_decode($data_json) ;
+
+$bargraph_height = 161 + $data->temperature*4;
+$bargraph_top = 315 - $data->temperature*4;
+
 ?>
 
 Il fait <?php echo $data->temperature ; ?>°C avec <?php echo $data->humidite ; ?>% d'humidité.</br>
 
-<div id="thermometer"> <div id="bargraph"></div> </div>
+<div id="thermometer"> <div id="bargraph" style="height: <?php echo $bargraph_height;?>px; top: <?php echo $bargraph_top; ?>px;"></div></div>
 
 
 </br>
-
-<!-- 	<img src="img/thermometer.jpg"> -->
-
 
 
 
