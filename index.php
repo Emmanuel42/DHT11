ac1ba1516feb12536b4888230d26799bd937f2e7
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-11-30 11:43:41 +0100

    Intégration du thermomètre à zéro degré

--- index.php
+++ index.php
@@ -9,12 +9,26 @@
 <body>
 
 	<h1>Température</h1>
-		
-
-<?php
 
 
+<?php 
+
+$filename = "data.txt"; 
+$data_json = file_get_contents($filename) ; 
+$data = json_decode($data_json) ;
 ?>
+
+Il fait <?php echo $data->temperature ; ?>°C avec <?php echo $data->humidite ; ?>% d'humidité.</br>
+
+<div id="thermometer"> <div id="bargraph"></div> </div>
+
+
+</br>
+
+<!-- 	<img src="img/thermometer.jpg"> -->
+
+
+
 
 </body>
 </html>
