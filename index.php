a76b59a1756133ce09501f77d86edc8dcd2e5fd4
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-12-06 09:00:22 +0100

    affiche date de dernière modification du fichier data.txt

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 
 	<h1>Température</h1>
 
-
 <?php 
 
 $filename = "data.txt"; 
@@ -20,9 +19,21 @@ $data = json_decode($data_json) ;
 $bargraph_height = 161 + $data->temperature*4;
 $bargraph_top = 315 - $data->temperature*4;
 
+//echo "$filename a été modifié pour la dernière fois le : ".date("F d Y à H:i:s.", filectime($filename));
+
+//met l'heure locale (fr)
+setlocale(LC_TIME, "fr_FR");
+
 ?>
 
-Il fait <?php echo $data->temperature ; ?>°C avec <?php echo $data->humidite ; ?>% d'humidité.</br>
+Il fait <?php echo $data->temperature ; ?>°C avec <?php echo $data->humidite ; ?>% d'humidité.<br>
+
+<!-- Date de dernière modification du fichier -->
+Le fichier <?php echo"$filename"; ?> a été modifié pour la dernière fois le : <?php echo date("d/m/Y à H:i:s.", filemtime($filename))."<br>"; ?>
+
+<!-- Ajout de la date du jour et de l'heure (fr) -->
+Nous sommes le <?php echo strftime("%d/%m/%Y"); ?>, il est : <?php echo strftime("%Hh %Mm %Ss"); ?>
+
 
 <div id="thermometer"> <div id="bargraph" style="height: <?php echo $bargraph_height;?>px; top: <?php echo $bargraph_top; ?>px;"></div></div>
 
