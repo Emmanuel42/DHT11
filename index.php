8448ab1b65e00b6a61f8130e8409e6df00a766ef
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-12-07 11:10:13 +0100

    lien entre thermomètre et fichier data.json et rafraichissement de page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>DHT11</title>
 	<meta charset="utf-8">
+	<meta http-equiv="refresh" content="1">
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
@@ -12,17 +13,12 @@
 
 <?php 
 
-$filename = "data.txt"; 
+$filename = "data.json"; 
 $data_json = file_get_contents($filename) ; 
-$data = json_decode($data_json) ;
+$data = json_decode($data_json);
 
 $bargraph_height = 161 + $data->temperature*4;
 $bargraph_top = 315 - $data->temperature*4;
-
-//echo "$filename a été modifié pour la dernière fois le : ".date("F d Y à H:i:s.", filectime($filename));
-
-//met l'heure locale (fr)
-setlocale(LC_TIME, "fr_FR");
 
 ?>
 
@@ -31,8 +27,6 @@ Il fait <?php echo $data->temperature ; ?>°C avec <?php echo $data->humidite ; 
 <!-- Date de dernière modification du fichier -->
 Le fichier <?php echo"$filename"; ?> a été modifié pour la dernière fois le : <?php echo date("d/m/Y à H:i:s.", filemtime($filename))."<br>"; ?>
 
-<!-- Ajout de la date du jour et de l'heure (fr) -->
-Nous sommes le <?php echo strftime("%d/%m/%Y"); ?>, il est : <?php echo strftime("%Hh %Mm %Ss"); ?>
 
 
 <div id="thermometer"> <div id="bargraph" style="height: <?php echo $bargraph_height;?>px; top: <?php echo $bargraph_top; ?>px;"></div></div>
